65ece5dfe031b7bd794d674b480d997fdde130d8
Maor-Levin <levinmaorlh@gmail.com>
2024-11-17 18:22:26 +0200

    done in 2 ways, classic C and repeat() method

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+// const repeatString = function(word,times) {
+//     result = ''
+//     if (times<0) return 'ERROR';
+//     for (let i=0 ; i<times ; i++){
+//         result = result + word ;
+//     }
+//     return result;
+// };
 
+const repeatString = function(word,times) {
+    if (times<0) return 'ERROR';
+    return word.repeat(times);
+    
 };
 
 // Do not edit below this line
