c5ca0df1dbd9b31ac49b43d09e6786a1ca734227
Maor-Levin <levinmaorlh@gmail.com>
2024-11-17 18:33:09 +0200

    done in 2 ways, C style and JS style

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+// const reverseString = function(word) {
+//     return word.split('').reverse().join('')
+// };
 
+const reverseString = function(word) {
+    let len = word.length;
+    res =''
+    for( i=len-1 ; i>=0 ; i-- ){
+        res = res + word[i];
+    }
+    return res;
 };
 
 // Do not edit below this line
