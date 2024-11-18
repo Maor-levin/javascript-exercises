729432a7b4c9867d4f0455d73d86f0acc5c7cd0c
Maor-Levin <levinmaorlh@gmail.com>
2024-11-18 22:25:44 +0200

    completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let tempC = (tempF-32)*(5/9);
+  tempRounded = Math.round(tempC*10) / 10;
+  return tempRounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let tempF = ((tempC * (9/5)) +32);
+  tempRounded = Math.round(tempF*10) / 10;
+  return tempRounded;
 };
 
 // Do not edit below this line
