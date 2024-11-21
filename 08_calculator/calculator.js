9d3376630ce6f7f5e53a5569dcebae28ffbc469c
Maor-Levin <levinmaorlh@gmail.com>
2024-11-21 13:48:29 +0200

    completed math functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(...numbers) {
+    return numbers.reduce((acc,num) => {
+      return acc + num;
+},0)
 };
 
-const subtract = function() {
-	
+const subtract = function(...numbers) {
+    return numbers.slice(1).reduce((acc, num) => {
+      return acc - num;
+    },numbers[0])
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  return numbers.reduce((acc,num) => {
+    return acc + num;
+},0)
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((acc,num) => {
+    return acc * num;
+},1)
 };
 
-const power = function() {
-	
+const power = function(num,power) {
+  if (power == 0 ) return 1;
+  if (power == 1 ) return num;
+  let sum = num;
+
+	for (let i = 2 ; i <=power ; i++ ){
+    sum = sum*num;
+  }
+  return sum;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if ( num == 0 ) return 1;
+  if ( num == 1 ) return 1;
+  let result = 1;
+  for (let i = 1 ; i<= num ; i++)
+    result *= i;
+  return result
 };
 
 // Do not edit below this line
